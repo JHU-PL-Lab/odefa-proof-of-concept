1b78477120acb576ae619f9fce1b3c48cadbc46a
Zachary Palmer <zep_git@bahj.com>
2015-06-12 09:31:49 -0400

    Changed output type of lookup.

--- src/odefa-analysis/odefa_analysis.ml
+++ src/odefa-analysis/odefa_analysis.ml
@@ -66,7 +66,7 @@ struct
   type s = S.s;;
   
   let lookup g init_x init_acl0 =
-    let rec lookup' x acl0 lookups context : value Enum.t =
+    let rec lookup' x acl0 lookups context : Value_set.t =
       (* For each predecessor of acl0, we need to look at the form of the
          clause. *)
       preds g acl0
@@ -74,7 +74,7 @@ struct
             (fun acl1 ->
               match acl1 with
                 | Start_clause | End_clause ->
-                    Enum.empty ()
+                    Value_set.empty
                 | Annotated_clause(Clause(x_here,b)) ->
                     if x_here = x then
                       begin
@@ -84,7 +84,7 @@ struct
                                 match lookups with
                                   | [] ->
                                     (* Rule #1 applies: this is the answer! *)
-                                    Enum.singleton v
+                                    Value_set.singleton v
                                   | Function_lookup(x1) :: lookups_t ->
                                       begin
                                         match v with
@@ -105,7 +105,7 @@ struct
                                                  indicate a type error, that
                                                  will be caught later and should
                                                  not be handled here. *)
-                                              Enum.empty ()
+                                              Value_set.empty
                                       end
                               end
                           | Var_body(x') ->
@@ -115,7 +115,7 @@ struct
                               lookup' x' acl1 lookups context
                           | Appl_body(_,_) | Conditional_body(_,_,_,_) ->
                               (* We do not move backward through these nodes. *)
-                              Enum.empty ()
+                              Value_set.empty
                       end
                     else
                       (* Rule #6 applies: this clause has nothing to do with
@@ -157,7 +157,7 @@ struct
                     else
                       (* This entry point doesn't correspond to our context.
                          We can skip it. *)
-                      Enum.empty ()
+                      Value_set.empty
                 | Exit_clause(x_here,x_from,site) ->
                     if x_here = x then
                       (* Rule #5 applies: we're looking at a function's
@@ -168,12 +168,11 @@ struct
                          variable.  We can skip the entire function body because
                          there's no way that it will affect the thing we are
                          trying to look up (due to lack of side effects!). *)
-                      Enum.empty ()
+                      Value_set.empty
             )
-        |> Enum.concat
+        |> Enum.fold Value_set.union Value_set.empty
     in
     lookup' init_x init_acl0 [] S.empty
-      |> Value_set.of_enum
   ;;
 
 end;;
