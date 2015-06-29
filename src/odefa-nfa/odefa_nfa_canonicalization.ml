589479ddde25eab3418c3fed56cece4769c49aed
Zachary Palmer <zep_git@bahj.com>
2015-06-29 09:35:01 -0400

    Simplified NFA canonicalization functor spec.

--- src/odefa-nfa/odefa_nfa_canonicalization.ml
+++ src/odefa-nfa/odefa_nfa_canonicalization.ml
@@ -23,30 +23,14 @@ sig
   (** The generating function. *)
   val generate_nfa : t -> (state,symbol) nfa;;
 
-  (** An ordering on the generating type. *)
-  module Gen_ord : BatInterfaces.OrderedType with type t = t;;
   (** A hash on the generating type. *)
   module Gen_hash : Hashtbl.HashedType with type t = t;;
-  (** An ordering on the state type. *)
-  module State_ord : BatInterfaces.OrderedType with type t = state;;
-  (** An ordering on the symbol type. *)
-  module Symbol_ord : BatInterfaces.OrderedType with type t = symbol;;
 end;;
 
 module Make(S : Spec) =
 struct
   module Gen_hashtbl = Hashtbl.Make(S.Gen_hash);;
   
-  module Nfa_spec =
-  struct
-    type state = S.state
-    type symbol = S.symbol
-    module State_order = S.State_ord;;
-    module Symbol_order = S.Symbol_ord;;
-  end;;
-
-  module Nfa = Odefa_nfa.Make(Nfa_spec);;
-
   let gen_answer_cache : S.t Gen_hashtbl.t = Gen_hashtbl.create 10;;
 
   let viewed_nfa_answers : ((S.state,S.symbol) nfa * S.t) list ref = ref [];;
