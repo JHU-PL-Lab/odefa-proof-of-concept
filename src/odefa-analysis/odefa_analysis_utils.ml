196a19dc3a529e86796076f8862598342c517771
Zachary Palmer <zep_git@bahj.com>
2015-07-02 14:09:20 -0400

    Added a utility for extracting the variables from an expression.
    Added temp files to .gitignore

--- src/odefa-analysis/odefa_analysis_utils.ml
+++ src/odefa-analysis/odefa_analysis_utils.ml
@@ -4,9 +4,33 @@ open Batteries;;
 
 open Odefa_ast;;
 
-(* The clauses pushed onto a context stack are always invokable clauses
-   appearing within original expression (provided here).  Begin by finding
-   a set of those. *)
+(** Obtain the set of all variables appearing within an expression. *)
+let rec find_all_vars (Expr cls) =
+  cls
+  |> List.enum
+  |> Enum.map
+      (fun (Clause(x,r)) ->
+        match r with
+          | Value_body(_) -> Enum.singleton x
+          | Var_body(x') -> List.enum [x;x']
+          | Appl_body(x',x'') -> List.enum [x;x';x'']
+          | Conditional_body(x,_,f1,f2) ->
+            Enum.concat @@ List.enum @@
+              [ Enum.singleton x
+              ; find_all_vars_in_fn f1
+              ; find_all_vars_in_fn f2
+              ]
+          | Projection_body(x',_) -> List.enum [x;x']
+      )
+  |> Enum.concat
+
+and find_all_vars_in_fn (Function_value(x,e)) =
+  Enum.append (Enum.singleton x) @@ find_all_vars e
+;;
+
+(** Retrieve the set of "context clauses" appearing anywhere within an
+    expression.  These are clauses which may be pushed onto the context stack
+    during analysis. *)
 let rec extract_context_clauses (Expr cls) =
   let immediate =
     cls
